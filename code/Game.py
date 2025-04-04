0135b902bfc1a3b311c895748b79676b01a88cd2
vivi <vjsanto.viviane@gmail.com>
2025-04-04 12:39:37 -0300

    creating assets folder, insert image and sound menu, creating Const.py, insert variables menu, Creating background, sound and fonts menu

--- code/Game.py
+++ code/Game.py
@@ -1,12 +1,13 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT))
 
     def run(self):
         # chamar  aa classe menu
@@ -14,6 +15,3 @@ class Game:
             menu = Menu(self.window)
             menu.run()
 
-        #    for event in pygame.event.get():
-        #        if event.type == pygame.QUIT:
-    #             pygame.quit()
--- code/Menu.py
+++ code/Menu.py
@@ -1,12 +1,43 @@
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
 
+from code.Const import WIN_WIDTH, COLOR_ORANGE, COLOR_WHITE, MENU_OPTION
 
 
 class Menu:
 
-    #construtor
-    def __init__(self,window):
+    # construtor
+    def __init__(self, window):
         self.window = window
-
+        self.surf = pygame.image.load('./asset/menu.png')  # load image
+        self.rect = self.surf.get_rect(left=0, top=0)  # create reactangle to get image, default 0,0,0, init top left
 
     def run(self):
-        pass
+        # load sound
+        pygame.mixer.music.load('./asset/menu.mp3')
+        pygame.mixer.music.play(-1)  # play sound (-1) alltime sound
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)  # update image method run - Draw
+            self.menu_text(50, "Montain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 110))  # draw text
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()  # update window
+
+            # Check events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # closet window
+                    print('Closeting.....')
+                    quit()  # end pygame
+
+    # method text
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucid SansTypewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
